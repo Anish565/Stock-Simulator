6b88ad2373ff30b5c8bd4bddbe98d4005dd5a7d7
Akshat Shaha <akshatshaha@10-18-170-161.dynapool.wireless.nyu.edu>
2024-11-14 11:36:42 -0500

    fixed getTrending stocks and updated package.json

--- streaming-finance-data/src/controllers/trendingController.js
+++ streaming-finance-data/src/controllers/trendingController.js
@@ -4,13 +4,13 @@ const yahooFinance = require('yahoo-finance2').default
 
 async function getTrendingStocks(req, res) {
     try {
-        logger.info(`getTrendingStocks: Received request ${JSON.stringify(req)}`);
+        //logger.info(`getTrendingStocks: Received request ${JSON.stringify(req)}`);
         const trendingData = await yahooFinance.trendingSymbols('US');
         logger.info(`getTrendingStocks: Successfully fetched trending stocks data ${trendingData}`);
         res.json(trendingData);
     } catch (error) {
-        logger.error(`getTrendingStocks: Error fetching trending stocks - ${err.message}`);
-        res.status(500).json({ error: `Failed to fetch trending stocks. ${err.message}`});
+        logger.error(`getTrendingStocks: Error fetching trending stocks - ${error.message}`);
+        res.status(500).json({ error: `Failed to fetch trending stocks. ${error.message}`});
     }
     
 }
