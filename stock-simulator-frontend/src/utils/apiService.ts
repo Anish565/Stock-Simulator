d89fce918bcfee217e6b13df67b79a7cff13ee3f
Anish565 <anishnimbalkar@gmail.com>
2024-11-23 14:57:49 -0500

    apiservice.ts

--- stock-simulator-frontend/src/utils/apiService.ts
+++ stock-simulator-frontend/src/utils/apiService.ts
@@ -4,13 +4,26 @@ const apiEndpoint = process.env.APIENDPOINT;
 
 export const registerUser = async (username: string, email: string, password: string) => {
     try {
-        const response = await axios.post(`${apiEndpoint}/users/register`, {
+        const response = await axios.post(`${apiEndpoint}/register`, {
             body: { username, email, password },
         });
         console.log("User registered successfully:", response.data);
         return response.data;
     } catch (error) {
         console.error("Error registering user:", error);
+        throw error;
+    }
+}
+
+export const loginUser = async (email: string, password: string) => {
+    try {
+        const response = await axios.post(`${apiEndpoint}/login`, {
+            body: { email, password },
+        });
+        console.log("User logged in successfully:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error logging in user:", error);
         throw error;
     }
 }
