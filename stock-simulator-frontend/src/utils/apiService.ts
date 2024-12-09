665e42faeba1d5f17d9c5dfda37e4379ade9ec45
Somya Gupta <somyag00@gmail.com>
2024-12-09 16:17:55 -0500

    Sell implemented using websocket

--- stock-simulator-frontend/src/utils/apiService.ts
+++ stock-simulator-frontend/src/utils/apiService.ts
@@ -335,7 +335,7 @@ export const buyStock = async (sessionId: string, symbol: string, quantity: numb
       symbol,
       quantity,
       price,
-      current_market_vol: volume
+      current_market_vol: 1034839 // Dummy value as requested
     },
     {
       headers: {
